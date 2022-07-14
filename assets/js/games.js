4c4c23dd0e0d17305220dd267cafd41b57a04879
srich92 <srichards646@gmail.com>
2022-07-13 22:41:07 -0400

    functions

--- assets/js/games.js
+++ assets/js/games.js
@@ -1,2 +1,18 @@
+var playerName = window.prompt("What is your robot's name?");
+// What is this?
+console.log(playerName);
+console.log("This logs a string, good for leaving yourself a message");
+// this will do math and log 20
+console.log(10 + 10);
+// what is this?
+console.log("Our robot's name is " + playerName);
 
-window.alert("This is an alert! JavaScript is running!");
+var name = "your name";
+console.log(name);
+
+// this creates a function named "fight"
+function fight() {
+  window.alert("The fight has begun!");
+}
+fight()
+//fight();
